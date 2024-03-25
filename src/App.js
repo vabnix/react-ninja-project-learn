d9752e26a933e1f2ed0479f459c1f68cc65757bf
Vaibhav Srivastava <vabnix@hotmail.com>
2024-03-25 15:34:46 -0500

    Adding react code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import Ninja from './Ninja';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first React App</h1>
+      <p>Welcome!</p>
+      <Ninja />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Ninja.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class Ninja extends Component {
+    render() {
+        return (
+            <div className="ninja">
+                <div>Name: Vaibhav</div>
+                <div>Age: 40</div>
+                <div>Belt: Black</div>
+            </div>
+        )
+
+    }
+}
+
+export default Ninja;

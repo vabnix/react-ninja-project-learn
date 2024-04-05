aea54012a86b974bd5f60bc1a2f5057065fecddc
Vaibhav Srivastava <vabnix@hotmail.com>
2024-04-04 19:14:55 -0500

    adding bootstrap stylesheet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 import Ninja from "./Ninja";
+import Navbar from "./navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first React App</h1>
-      <p>Welcome!</p>
-      <Ninja name="Pallabi" age="40" belt="Yellow" />
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Pallabi", age: 40, belt: "black", id: 1 },
+      { name: "Vaibhav", age: 40, belt: "black", id: 2 },
+      { name: "Aum", age: 7, belt: "black", id: 3 },
+      { name: "Ishaan", age: 4, belt: "black", id: 4 },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+      <Navbar/>
+        <div className="jumbotron">
+          <h1 className="display-4">Hello, world!</h1>
+          <p className="lead">
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <hr className="my-4" />
+        </div>
+        <Ninja ninjas={this.state.ninjas} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Ninja.js
+++ src/Ninja.js
@@ -1,15 +1,25 @@
-import React, { Component } from "react";
-class Ninja extends Component {
-  render() {
-    const { name, age, belt } = this.props;
-    return (
-      <div className="ninja">
-        <div>Name: {name}</div>
-        <div>Age: {age}</div>
-        <div>Belt: {belt}</div>
-      </div>
-    );
-  }
+import React from "react";
+
+const Ninja = (props) => {
+
+    const { ninjas } = props;
+    const ninjaList = ninjas.map((ninja) => {
+      return (
+        <div className="form-row align-items-center ml-2 mb-2" key={ninja.id}>
+          <div className="col-md-1">Name: {ninja.name}</div>
+          <div className="col-md-1">Age: {ninja.age}</div>
+          <div className="col-md-1">Belt: {ninja.belt}</div>
+          <div className="col-auto">
+            <i type="submit" className="bi bi-pencil"></i>
+          </div>
+          <div className="col-auto">
+            <i type="submit" className="bi bi-trash"></i>
+          </div>
+        </div>
+      );
+    });
+    return <form>{ninjaList}</form>;
+
 }
 
 export default Ninja;
